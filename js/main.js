ffc890929b4fb59f4a01d2e751e2e7133954b57b
Coleman Kelleghan <imdesigner2012@gmail.com>
2017-06-21 15:09:46 -0600

    Done

--- js/main.js
+++ js/main.js
@@ -1,1 +1,17 @@
 // Chuck norris code goes here
+
+$( document ).ready( function(){
+
+  function appendJokes(id, joke){
+    $('#target').append('<div class="joke"><span class="joke-id">ID: ' + id + ' </span> - ' + joke + '</div>');
+  }
+
+  $.get('https://api.icndb.com/jokes/random/6', function(res){
+
+    for (var i = 0; i < res.value.length; i++) {
+      appendJokes(res.value[i].id, res.value[i].joke);
+    }
+
+  });
+
+});
